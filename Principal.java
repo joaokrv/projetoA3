51cba8a28b7658ce86dabb6fd922f93c0f787f2a
João Victor Carvalho <142843652+joaokrv@users.noreply.github.com>
2024-06-14 03:50:09 -0300

    Update Principal.java

--- Principal.java
+++ Principal.java
@@ -1,50 +1,69 @@
 import java.io.IOException;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Principal {
     public static void main(String[] args) throws IOException {
-    
         Scanner in = new Scanner(System.in);
         Estoque estoque = new Estoque();
         Produtos produto = new Produtos("Teste", 999, 1, "Teste", 01, 01, 0, 0);
         int opcao;
-        
+        boolean continuar = true;
+
         do {
-            System.out.println("\nMenu Principal:");
+            System.out.println("\nMenu Principal:\n-------------------------");
             System.out.println("1. Adicionar Produto");
             System.out.println("2. Remover Produto");
-            System.out.println("3. Buscar Produto");
-            System.out.println("4. Listar Produtos");
-            System.out.println("5. Atualizar Produto");
-            System.out.println("6. Sair");
-            System.out.print("Digite sua opção: ");
+            System.out.println("3. Listar Produtos");
+            System.out.println("4. Atualizar Produto");
+            System.out.println("5. Sair\n-------------------------");
+            System.out.print("Digite sua opção > ");
+            String opcaoStr = in.next();
 
-            opcao = in.nextInt();
+            try {
+                opcao = Integer.parseInt(opcaoStr);
+            } catch (NumberFormatException  e) {
+                System.out.println("Opção inválida. Digite um número inteiro.");
+                continue;
+            }
 
             switch (opcao) {
                 case 1:
-                    estoque.addProduto(produto);
+                    try {
+                        estoque.addProduto();
+                    } catch (InputMismatchException  e) {
+                        System.err.println("Erro ao adicionar produto: " + e.getMessage());
+                    }
                     break;
                 case 2:
-                    estoque.removeProduto(produto);
+                    try {
+                        estoque.removeProduto(produto);
+                    } catch (IllegalArgumentException e) {
+                        System.err.println("Erro ao remover produto: " + e.getMessage());
+                    }
                     break;
                 case 3:
-                    estoque.buscarProduto(produto.getName(), produto.getId());
-                    break;
-                case 4:
                     estoque.listarProdutos();
                     break;
-                case 5:
-                    estoque.atualizarProduto(produto);
+                case 4:
+                    try {
+                        estoque.atualizarProduto(produto);
+                    } catch (IOException e) {
+                        System.err.println("Erro ao atualizar produto: " + e.getMessage());
+                    }
                     break;
-                case 6:
+                case 5:
                     System.out.println("Saindo...");
                     break;
                 default:
                     System.out.println("Opção inválida. Tente novamente.");
             }
-        } while (opcao != 6);
-            
+            if (opcao == 5) {
+                continuar = false;
+              }
+
+        } while (continuar);
+
         in.close();
     }
 }
